e5205a894be894f7e2a8339d1fa5ad48c492a5df
Itamar Mizrahi <48559693+itamar-mizrahi@users.noreply.github.com>
2021-10-29 15:29:33 +0300

    add list of contacts

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import { useState } from 'react'
 
-import {Alex} from '../../../Assets/alex jonathan.jpg'
+import Alex from '../Assets/alex-jonathan.jpg'
+import Janeth from '../Assets/janeth carton.jpg'
+import John from '../Assets/michael zimber.jpg'
+import Michael from '../Assets/john-smith.jpg'
+import Monica from '../Assets/monica smith.jpg'
+import Sandra from '../Assets/sandra smith.jpg'
 export const Contact = () => {
 
     const data = [
-        { id: 1, name: "alex jonathan" },
-        { id: 2, name: "Victor Wayne" },
-        { id: 3, name: "Jane Doe" },
-      ];
-
-      const imageSizes = [
-        { name: "horizontal", width: 600, height: 380 },
-        { name: "vertical", width: 400, height: 650 },
-        { name: "thumbnail", width: 300, height: 300 },
+        { id: 1, name: "alex jonathan", src:Alex },
+        { id: 2, name: "janeth carton", src:Janeth },
+        { id: 3, name: "michael zimber" , src:Michael},
+        { id: 4, name: "john smith" , src:John },
+        { id: 5,  name: "monica smith" ,src:Monica },
+        { id: 6,  name: "sandra smith" ,src:Sandra }
       ];
     const [contact, setContact] = useState(0);
     return (
         <div className="users">
       {data.map((user) => (
-        <div className="user">{user}</div>
-      ))}
+        <div className="user">{user.id}.{user.name}
+        <img className="profile-photo" src={user.src} alt={user.name}/>
+        </div>
+        ))}
     </div>
+    
     )
 }
